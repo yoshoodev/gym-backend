bb6c759817cd892bedabfcbf1cfccaa19b640d12
yoshoodev <90975875+yoshoodev@users.noreply.github.com>
2022-08-10 15:16:41 +0300

    Added Type, Query and Mutation for User type.

--- src/schema.js
+++ src/schema.js
@@ -1,7 +1,28 @@
 const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
-    # Your schema will go here
+    enum UserTypes {
+        MEMBER
+        TRAINER
+        ADMIN
+    }
+    
+    type User {
+        id: ID!
+        role: UserTypes!
+        name: String
+        surname: String
+        email: String!
+    }
+    
+    type Query {
+        users: [User]
+    }
+    
+    type Mutation {
+        addUser(email: String!, name: String, surname: String) : User
+    }
+    
 `;
 
 module.exports = typeDefs;
