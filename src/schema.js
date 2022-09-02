aadad0b46f7d34c386dfc480754efe9e2626195a
yoshoodev <90975875+yoshoodev@users.noreply.github.com>
2022-09-02 13:41:50 +0300

    Added countries enum, fixed export + more.

--- src/schema.js
+++ src/schema.js
@@ -4,25 +4,34 @@ const typeDefs = gql`
     enum UserTypes {
         MEMBER
         TRAINER
+        OWNER
         ADMIN
+    }
+    
+    enum Countries {
+        RO
     }
     
     type User {
         id: ID!
         role: UserTypes!
+        email: String!
         name: String
         surname: String
-        email: String!
+        country: Countries
+        phoneNumber: String
     }
     
     type Query {
         users: [User]
+        user(id: ID, email: String): User
     }
     
     type Mutation {
-        addUser(email: String!, name: String, surname: String) : User
+        addUser(role: UserTypes, email: String): User 
     }
-    
 `;
 
-module.exports = typeDefs;
+module.exports = {
+    typeDefs,
+}
