7b9cd04a925d6afc79bd5025a54f38f5aa154e92
yoshoodev <90975875+yoshoodev@users.noreply.github.com>
2022-09-02 13:43:10 +0300

    Added resolvers for user and users query.

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,4 +1,23 @@
-const Query = {}
+const Query = {
+    users(_parent, _args, context, _info) {
+        return context.prisma.member.findMany()
+    },
+    user(_parent, args, context, _info) {
+        if (args.id) {
+            return context.prisma.member.findUnique({
+                where: {
+                    id: args.id,
+                },
+            })
+        } else {
+            return context.prisma.member.findFirst({
+                where: {
+                    email: args.email,
+                }
+            })
+        }
+    },
+}
 
 module.exports = {
     Query,
