c5580cb33c28b1a5626b1eb407d06260c6c07801
yoshoodev <90975875+yoshoodev@users.noreply.github.com>
2022-09-03 11:25:54 +0300

    Update schema to match DB

--- src/schema.js
+++ src/schema.js
@@ -20,15 +20,51 @@ const typeDefs = gql`
         surname: String
         country: Countries
         phoneNumber: String
+        #address
+        street: String
+        number: String
+        city: String
+        postalCode: String
+        branch: Branch
+    }
+    
+    type Gym {
+        id: ID!
+        name: String
+        country: Countries
+        branches: [Branch]
+    }
+    
+    type Branch {
+        id: ID!
+        gym: Gym!
+        name: String!
+        members: [User]
+        phoneNumber: String
+        street: String
+        number: String
+        city: String
+        country: Countries
+        postalCode: String
     }
     
     type Query {
+        "Returns all users."
         users: [User]
+        "Returns all branches."
+        branches: [Branch]
+        "Returns all gyms."
+        gyms: [Gym]
+        "Queries User by ID or email. ID is preferred as it uses findUnique."
         user(id: ID, email: String): User
+        "Queries Branch by ID."
+        branch(id: ID): Branch
     }
     
     type Mutation {
-        addUser(role: UserTypes, email: String): User 
+        addUser(role: UserTypes, name: String, surname: String, email: String!,country: Countries, phoneNumber: String, street: String, number: String, city: String, postalCode: String, branchId: String): User
+        addGym(name: String!, country: Countries): Gym
+        
     }
 `;
 
